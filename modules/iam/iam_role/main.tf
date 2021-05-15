100c1d0873cadce7fb3821e75e5f039f2bc0bb2b
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-15 11:15:33 +0100

    add ssm iam permission

--- modules/iam/iam_role/main.tf
+++ modules/iam/iam_role/main.tf
@@ -3,3 +3,8 @@ resource "aws_iam_role" "environment" {
   assume_role_policy = "${file("${path.module}/policies/${var.policyfile}")}"
   description        = "${var.rolename}"
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_agent" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.environment.name
+}
