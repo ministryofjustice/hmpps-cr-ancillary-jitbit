c5111c0a504e49b94d67f1e36e3a09a248e2f88b
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-17 16:07:03 +0100

    update iam permission

--- modules/iam/iam_role/main.tf
+++ modules/iam/iam_role/main.tf
@@ -8,3 +8,8 @@ resource "aws_iam_role_policy_attachment" "ssm_agent" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.environment.name
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_ad_agent" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMDirectoryServiceAccess"
+  role       = aws_iam_role.environment.name
+}
