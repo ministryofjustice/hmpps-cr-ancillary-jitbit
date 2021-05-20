d46cc236b33772fbcb6a86ef9199ea5191219e07
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-20 11:38:30 +0100

    remove samba and efs setup as move to fsx and ssm

--- efs/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  jitbit_efs_configs = merge(var.jitbit_efs_configs, var.jitbit_efs_overrides)
-  tags               = data.terraform_remote_state.common.outputs.tags
-  kms_key_arn        = data.terraform_remote_state.common.outputs.kms_arn
-  common_name        = data.terraform_remote_state.common.outputs.common_name
-  subnet_ids         = data.terraform_remote_state.common.outputs.db_subnet_ids
-  vpc_id             = data.terraform_remote_state.common.outputs.vpc_id
-}
