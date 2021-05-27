0ad6d433852eade4bc95546fbb6b94aecca0cb23
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-27 13:02:38 +0100

    add cloud watch files

---
+++ cloudwatch/s3object.tf
@@ -0,0 +1,16 @@
+locals {
+  common_name = data.terraform_remote_state.common.outputs.common_name
+}
+
+data "template_file" "cloudwatch_config" {
+  template = file("./files/config.json")
+  vars = {
+    log_group_name = local.common_name
+  }
+}
+
+resource "aws_s3_bucket_object" "cloudwatch_config" {
+  bucket = local.common_name
+  key = "/cloudwatch/config.json"
+  content = data.template_file.cloudwatch_config.rendered
+}
