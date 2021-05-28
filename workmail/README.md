f44c33a89e20ce3d12bf6012ad9aa77364712501
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-05-28 13:29:51 +0100

    remove route53 entry usage

--- workmail/README.md
+++
@@ -1,5 +0,0 @@
-# WorkMail
-* As of 21/May/2021 Terraform provider and london region dont support creation of WorkMail its setup manually through console
-* Separate Route53 for hosted zone entry created for inbound email to work with Work mail as SES configured to use default hosted zone
-* SSM holds the WorkMail credentials
-* Ireland region is used to setup WorkMail
--- workmail/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  environment        = replace("${var.environment_type}", "jitbit-", "")
-  subdomain          = "${local.environment == "dev" ? "mail.${local.environment}" : "mail" }"
-  mail_public_domain = "${local.subdomain}.${var.project_name}.probation.${var.public_dns_parent_zone}"
-  tags               = data.terraform_remote_state.common.outputs.tags
-}
