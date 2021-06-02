b38f289d0c95bbc8b57d77090db7d084b3531178
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-02 15:07:02 +0100

    add iss httperr alarm

--- alerts/instance.tf
+++ alerts/instance.tf
@@ -2,13 +2,29 @@
 resource "aws_cloudwatch_log_metric_filter" "iis_httperr_metrics" {
   name           = "IIS_AppPool"
   pattern        = "AppOffline"
-  log_group_name = "${local.common_name}"
+  log_group_name = local.common_name
 
   metric_transformation {
     name      = "AppPool"
     namespace = "IIS"
     value     = "1"
   }
+}
+
+# IIS HttpErr Alarm
+resource "aws_cloudwatch_metric_alarm" "iis_httperr" {
+  alarm_name                = "${local.common_name}_AppPool_Offline"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "AppPool"
+  namespace                 = "IIS"
+  period                    = "60"
+  statistic                 = "SampleCount"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors IIS HttpErr"
+
+#   alarm_actions       = [aws_sns_topic.alarm_notification.arn]
+#   ok_actions          = [aws_sns_topic.alarm_notification.arn]
 }
 
 # General Instance check 
