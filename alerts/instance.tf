b483ddbfc5d133fe11b07ff1ebe110109ca21c18
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-02 14:48:50 +0100

    add cloudwatch custom metrics and skelton

--- alerts/instance.tf
+++ alerts/instance.tf
@@ -2,17 +2,13 @@
 resource "aws_cloudwatch_log_metric_filter" "iis_httperr_metrics" {
   name           = "IIS_AppPool"
   pattern        = "AppOffline"
-  log_group_name = aws_cloudwatch_log_group.iis_httperr_metrics.name
+  log_group_name = "${local.common_name}"
 
   metric_transformation {
     name      = "AppPool"
     namespace = "IIS"
     value     = "1"
   }
-}
-
-resource "aws_cloudwatch_log_group" "iis_httperr_metrics" {
-  name = "${local.common_name}/*/iis/httpErr"
 }
 
 # General Instance check 
