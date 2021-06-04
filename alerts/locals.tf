7386320f5bfbdd7773299760e6415c9e26967d00
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-04 14:31:32 +0100

    update cw alarm

--- alerts/locals.tf
+++ alerts/locals.tf
@@ -1,4 +1,4 @@
 locals {
   common_name                = data.terraform_remote_state.common.outputs.common_name
-  sns_alarm_notification_arn = data.terraform_remote_state.monitoring.outputs.aws_sns_topic_alarm_notification["arn"]
+  sns_alarm_notification_arn = tostring(data.terraform_remote_state.monitoring.outputs.aws_sns_topic_alarm_notification["arn"])
 }
