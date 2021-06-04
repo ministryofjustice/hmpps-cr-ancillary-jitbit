0a9a26c560068ee664374fc13acdd3b63074457e
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-04 14:57:56 +0100

    update cw dashboard

--- alerts/dashboard.tf
+++ alerts/dashboard.tf
@@ -9,7 +9,7 @@ data "template_file" "dashboard" {
     common_prefix            = local.common_name
     lb_arn_suffix            = data.aws_lb.alb.arn_suffix
     target_group_arn_suffix  = data.aws_lb_target_group.target_group.arn_suffix
-    app_pool_httperr_offline = aws_cloudwatch_metric_alarm.iis_httperr.name
+    app_pool_httperr_offline = aws_cloudwatch_metric_alarm.iis_httperr.alarm_name
   }
 }
 
