c4a30ed90fb2c04f336d459414eb8902397c4d84
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-29 10:43:05 +0100

    update to use correct handler

--- modules/synthetics/main.tf
+++ modules/synthetics/main.tf
@@ -2,7 +2,7 @@ resource "aws_synthetics_canary" "synthetics" {
   name                 = var.common_name
   artifact_s3_location = var.artifact_s3_location
   execution_role_arn   = aws_iam_role.synthetics.arn
-  handler              = "index.handler"
+  handler              = "pageLoadBlueprint.handler"
   zip_file             = data.archive_file.synthetics.output_path
   runtime_version      = "syn-python-selenium-1.0"
 
