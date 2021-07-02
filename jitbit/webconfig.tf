a2eb95344a5330dcfec03f15f25e45e3513ef8db
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-07-02 17:05:41 +0100

    update config

--- jitbit/webconfig.tf
+++ jitbit/webconfig.tf
@@ -2,9 +2,9 @@ data "template_file" "webconfig" {
   template = file("./files/Web.config")
   vars = {
     app_url          = aws_route53_record.dns_entry.name
-    db_source        = local.database_endpoint
-    db_user_id       = data.aws_ssm_parameter.db_user_id.name
-    db_user_password = data.aws_ssm_parameter.db_user_password.name
+    db_source        = local.database_address
+    db_user_id       = data.aws_ssm_parameter.db_user_id.value
+    db_user_password = data.aws_ssm_parameter.db_user_password.value
   }
 }
 
