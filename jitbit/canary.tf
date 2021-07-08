b1150acb5e8bbd5eaf5b3fe196433e481732a1af
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-07-08 16:57:18 +0100

    refactor

--- jitbit/canary.tf
+++ jitbit/canary.tf
@@ -1,3 +1,11 @@
+locals {
+  security_group_list = [
+    aws_security_group.instance.id,
+    data.terraform_remote_state.common.outputs.sg_outbound_id,
+    local.fsx_integration_security_group_id
+  ]
+}
+
 module "active" {
   source = "../modules/asg"
 
@@ -10,24 +18,20 @@ module "active" {
     filesystem_dns_name      = data.terraform_remote_state.fsx.outputs.fsx.fsx_details["dns_name"]
     config_bucket            = local.config_bucket
     cloudwatch_config        = "cloudwatch/config.json"
-    image_id             = local.ami_id
-    instance_type        = local.jitbit_configs["instance_type"]
-    iam_instance_profile = module.iam-instance-profile.iam_instance_name
-    key_name             = local.ssh_deployer_key
-    security_groups = [
-      data.terraform_remote_state.common.outputs.sg_outbound_id,
-      aws_security_group.instance.id,
-      local.fsx_integration_security_group_id
-    ]
-    volume_size = local.jitbit_configs["volume_size"]
-    disk_size           = local.jitbit_configs["cache_disk_size"]
-    iops                  = local.jitbit_configs["cache_iops"]
-    device_name           = local.jitbit_configs["cache_device_name"]
-    volume_type           = local.jitbit_configs["cache_volume_type"]
-    subnet_ids                 = local.subnet_ids
-    min_size                  = local.jitbit_configs["asg_min_size"]
-    max_size                  = local.jitbit_configs["asg_max_size"]
-    desired_capacity          = local.jitbit_configs["asg_capacity"]
+    image_id                 = local.ami_id
+    instance_type            = local.jitbit_configs["instance_type"]
+    iam_instance_profile     = module.iam-instance-profile.iam_instance_name
+    key_name                 = local.ssh_deployer_key
+    security_groups          = local.security_group_list
+    volume_size              = local.jitbit_configs["volume_size"]
+    disk_size                = local.jitbit_configs["cache_disk_size"]
+    iops                     = local.jitbit_configs["cache_iops"]
+    device_name              = local.jitbit_configs["cache_device_name"]
+    volume_type              = local.jitbit_configs["cache_volume_type"]
+    subnet_ids               = local.subnet_ids
+    min_size                 = local.jitbit_configs["asg_min_size"]
+    max_size                 = local.jitbit_configs["asg_max_size"]
+    desired_capacity         = local.jitbit_configs["asg_capacity"]
   }
 
 }
