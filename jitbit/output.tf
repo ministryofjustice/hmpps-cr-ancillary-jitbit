68b11babc1ba575b3760c4763d240ba7e6ecc1e6
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-07-13 13:56:56 +0100

    remove old asg and lb

--- jitbit/output.tf
+++ jitbit/output.tf
@@ -1,13 +1,13 @@
 output "jitbit" {
   value = {
     security_group_id              = aws_security_group.instance.id
-    autoscaling_group_name         = aws_autoscaling_group.instance.name
-    aws_lb_name                    = aws_lb.instance.name
-    aws_lb_arn_suffix              = aws_lb.instance.arn_suffix
-    aws_lb_target_group_name       = aws_lb_target_group.instance.name
-    aws_lb_target_group_arn_suffix = aws_lb_target_group.instance.arn_suffix
+    # autoscaling_group_name         = aws_autoscaling_group.instance.name
+    aws_lb_name                    = aws_lb.jitbit.name
+    aws_lb_arn_suffix              = aws_lb.jitbit.arn_suffix
+    # aws_lb_target_group_name       = aws_lb_target_group.instance.name
+    # aws_lb_target_group_arn_suffix = aws_lb_target_group.instance.arn_suffix
     lb_security_group_id           = aws_security_group.lb.id
-    aws_route53_record_name        = aws_route53_record.dns_entry.name
+    aws_route53_record_name        = aws_route53_record.jitbit.name
     canary_sns_notification        = module.mgmt.aws_sns_topic_mgmt_notification["arn"]
   }
 }
