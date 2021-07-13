639901403dbc71103a3ccd042b639ba9af0805ea
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-07-13 14:28:30 +0100

    update reference to blue stack

--- jitbit/output.tf
+++ jitbit/output.tf
@@ -1,11 +1,11 @@
 output "jitbit" {
   value = {
     security_group_id              = aws_security_group.instance.id
-    # autoscaling_group_name         = aws_autoscaling_group.instance.name
+    autoscaling_group_name         = module.blue.asg["autoscaling_group_name"]
     aws_lb_name                    = aws_lb.jitbit.name
     aws_lb_arn_suffix              = aws_lb.jitbit.arn_suffix
-    # aws_lb_target_group_name       = aws_lb_target_group.instance.name
-    # aws_lb_target_group_arn_suffix = aws_lb_target_group.instance.arn_suffix
+    aws_lb_target_group_name       = module.blue.asg["aws_lb_target_group_name"]
+    aws_lb_target_group_arn_suffix = module.blue.asg["aws_lb_target_group_arn_suffix"]
     lb_security_group_id           = aws_security_group.lb.id
     aws_route53_record_name        = aws_route53_record.jitbit.name
     canary_sns_notification        = module.mgmt.aws_sns_topic_mgmt_notification["arn"]
--- modules/asg/output.tf
+++ modules/asg/output.tf
@@ -1,3 +1,8 @@
-output "target_group" {
-  value = aws_lb_target_group.instance.arn
+output "asg" {
+  value = {
+    aws_lb_target_group_arn  = aws_lb_target_group.instance.arn
+    aws_lb_target_group_arn_suffix = aws_lb_target_group.instance.arn_suffix
+    aws_lb_target_group_name = aws_lb_target_group.instance.name
+    autoscaling_group_name = aws_autoscaling_group.instance.name
+  }
 }
