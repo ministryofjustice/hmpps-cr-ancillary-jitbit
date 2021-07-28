65a32a065af19e1612c566842bd18c612abe7bbb
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-07-28 16:19:09 +0100

    update config file

--- configs/s3object.tf
+++ configs/s3object.tf
@@ -31,6 +31,9 @@ resource "aws_s3_bucket_object" "passive_cloudwatch_config" {
 # Logrotate config
 data "template_file" "logrotate_config" {
   template = file("./files/iis_log_rotation.xml")
+  vars = {
+    ad_name = local.common_name
+  }
 }
 
 resource "aws_s3_bucket_object" "logrotate_config" {
