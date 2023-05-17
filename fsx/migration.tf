3d6bad0c530379fbed9ce0239024734335574129
Seb Norris <sebastian@automationlogic.com>
2023-05-17 10:44:20 +0100

    use count to prevent failure in higher envs

--- fsx/migration.tf
+++ fsx/migration.tf
@@ -1,13 +1,16 @@
-#Create an IAM role called datasync_s3_role
 resource "aws_iam_role" "datasync_s3_role" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   name = "jitbit-datasync-transfer-to-s3"
   path = "/"
 
-  assume_role_policy = data.aws_iam_policy_document.datasync_s3_trust.json
+  assume_role_policy = data.aws_iam_policy_document.datasync_s3_trust[count.index].json
 }
 
 # Create the trust policy to allow DataSync to access the datasync_s3_role:
 data "aws_iam_policy_document" "datasync_s3_trust" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   statement {
     effect = "Allow"
 
@@ -22,6 +25,8 @@ data "aws_iam_policy_document" "datasync_s3_trust" {
 
 # Create the policy to allow DataSync to access S3
 data "aws_iam_policy_document" "datasync_s3_access" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   statement {
     effect = "Allow"
     actions = [
@@ -47,27 +52,35 @@ data "aws_iam_policy_document" "datasync_s3_access" {
 }
 
 resource "aws_iam_policy" "datasync_s3_policy" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   name   = "datasync_s3_policy"
-  policy = data.aws_iam_policy_document.datasync_s3_access.json
+  policy = data.aws_iam_policy_document.datasync_s3_access[count.index].json
 }
 
 # Attach policy to the role
 resource "aws_iam_policy_attachment" "datasync_s3_attachment" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   name       = "datasync_s3_attachment"
-  roles      = [aws_iam_role.datasync_s3_role.name]
-  policy_arn = aws_iam_policy.datasync_s3_policy.arn
+  roles      = [aws_iam_role.datasync_s3_role[count.index].name]
+  policy_arn = aws_iam_policy.datasync_s3_policy[count.index].arn
 }
 
 resource "aws_datasync_location_s3" "bucket_location" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   s3_bucket_arn = "arn:aws:s3:::${local.fsx.migration_bucket_names[var.environment_name]}"
   subdirectory  = ""
 
   s3_config {
-    bucket_access_role_arn = aws_iam_role.datasync_s3_role.arn
+    bucket_access_role_arn = aws_iam_role.datasync_s3_role[count.index].arn
   }
 }
 
 resource "aws_datasync_location_fsx_windows_file_system" "fsx_location" {
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
   fsx_filesystem_arn  = module.fsx.fsx.file_system_arn
   subdirectory        = "/share/OfficeAdmin/"
   user                = "admin"
@@ -76,8 +89,10 @@ resource "aws_datasync_location_fsx_windows_file_system" "fsx_location" {
 }
 
 resource "aws_datasync_task" "fsx_to_s3_migration" {
-  destination_location_arn = aws_datasync_location_s3.bucket_location.arn
+  count = var.environment_name == "cr-jitbit-dev" ? 1 : 0
+
+  destination_location_arn = aws_datasync_location_s3.bucket_location[count.index].arn
   name                     = "fsx_to_s3_migration"
-  source_location_arn      = aws_datasync_location_fsx_windows_file_system.fsx_location.arn
+  source_location_arn      = aws_datasync_location_fsx_windows_file_system.fsx_location[count.index].arn
 
 }
